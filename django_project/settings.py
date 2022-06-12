202b4365aa55a2fd046f2d5517d3843859b36547
jnutterdev <hello@jnutterdev.com>
2022-06-12 09:34:21 -0400

    Added about page, install django-htmx

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
 
+    'django_htmx',
     'tailwind',
     'theme',
 ]
@@ -55,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'django_project.urls'
